68dbbe99ad94413ba70dc27856c08cf265a9ad3d
murilocaruy <42190902+murilocaruy@users.noreply.github.com>
2018-10-15 11:59:17 -0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,23 @@
 import pytest
 from business.animal import *
 
-def test_Gato():
+def test_Gato1():
     assert Animal("Mingau", Gato()).tomar_vacina(VacinaIntraMuscular())
 
-def test_Cachorro():
+def test_Cachorro1():
     assert Animal("Rex", Cachorro()).tomar_vacina(VacinaIntraVenosa())
 
-def test_Cavalo():
+def test_Cavalo1():
     assert Animal("Silver", Cavalo()).tomar_vacina(VacinaSuperficial())
+
+def test_Gato2():
+    with pytest.raises(TipoVacinaInvalido):
+        Animal("Garfield", Gato()).tomar_vacina(VacinaIntraVenosa())
+
+def test_Cachorro2():
+    with pytest.raises(TipoVacinaInvalido):
+        Animal("Laika", Cachorro()).tomar_vacina(VacinaIntraMuscular())
+
+def test_Cavalo2():
+    with pytest.raises(TipoVacinaInvalido):
+        Animal("Geronimo", Cavalo()).tomar_vacina(VacinaIntraVenosa())
