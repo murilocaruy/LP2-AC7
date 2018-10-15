f59b879616a8e41de2da0dbbf59d0b971c20d93c
murilocaruy <42190902+murilocaruy@users.noreply.github.com>
2018-10-15 11:55:46 -0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -2,10 +2,10 @@ import pytest
 from business.animal import *
 
 def test_Gato():
-    assert Animal("Mingau", Gato()).tomar_vacina(VacinaIntraMuscular()) == True
+    assert Animal("Mingau", Gato()).tomar_vacina(VacinaIntraMuscular())
 
 def test_Cachorro():
-    assert Animal("Rex", Cachorro()).tomar_vacina(VacinaIntraVenosa()) == True
+    assert Animal("Rex", Cachorro()).tomar_vacina(VacinaIntraVenosa())
 
 def test_Cavalo():
-    assert Animal("Silver", Cavalo()).tomar_vacina(VacinaSuperficial()) == True
+    assert Animal("Silver", Cavalo()).tomar_vacina(VacinaSuperficial())
